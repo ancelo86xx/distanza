f04f715e0fcf3aac345b350ea925d4f5af15f9af
Angelo Cante <bob@alice.it>
2022-02-21 11:52:43 +0100

    fix bug

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const geolib = require('geolib');
 
 let dist = geolib.getDistance(
     { latitude: 51.5103, longitude: 7.49347 },
-    { latitude: 51.5103, longitude: 7.49347 }
+    { latitude: 52.5103, longitude: 8.49347 }
 );
 console.log(dist)
